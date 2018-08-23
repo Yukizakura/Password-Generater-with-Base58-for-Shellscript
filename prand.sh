beeef330cdb14d2e2e8e7debb481fc5608ca9e52
yukizakura <sakurazaki.rikka@gmail.com>
2018-08-23 17:57:51 +0800

    Update and rename password_generater_base58.sh to prand.sh
    odコマンドを修正

--- prand.sh
+++ prand.sh
@@ -3,18 +3,23 @@
 # Password Generater with Base58
 # required upper and lower case letters, numbers
 # length 4 or more
-# ex.) $ password_generater_base58.sh 12
+# ex.) $ prand.sh 16
+
+# !環境によっては動作しない(mac, odコマンド)
 
 # input check
-char_num=8  # default length
+pw_length=12  # default length
 if [[ -nz "$1" && "$1" > 3 ]]; then
-	char_num="$1"
+	pw_length="$1"
 fi
 
 base58=(1 2 3 4 5 6 7 8 9 A B C D E F G H J K L M N P Q R S T U V W X Y Z a b c d e f g h i j k m n o p q r s t u v w x y z)
+base58m20=(1 2 3 4 5 6 7 8 9 A B C D E F G H J K L M N P Q R S T U V W X Y Z a b c d e f g h i j k m n o p q r s t u v w x y z \! \# \$ \% \& \( \) \* \+ \- \/ \< \= \> \? \@ \[ \] \{ \})
+
+
 
 while : ;do
-	str=$(od -vAn --width=4 -tu4 -N`expr $char_num \* 4` </dev/urandom | awk '{ print $1 % 58 }')
+	str=$(od -vA n --width=4 -t u4 -N `expr $pw_length \* 4` </dev/urandom | awk '{ print $1 % 58 }')
 	ary=(`echo $str`)
 
 	# initalize flag
